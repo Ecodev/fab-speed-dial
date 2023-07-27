bbe33e06dfe6a55439e4f8008d437581c703a59a
Adrien Crivelli <adrien.crivelli@gmail.com>
2023-07-27 13:09:14 +0200

    Prefix node core modules
    As is new best-practice

--- projects/fab-speed-dial/karma.conf.js
+++ projects/fab-speed-dial/karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
         },
         coverageIstanbulReporter: {
-            dir: require('path').join(__dirname, '../../coverage/fab-speed-dial'),
+            dir: require('node:path').join(__dirname, '../../coverage/fab-speed-dial'),
             reports: ['html', 'lcovonly', 'text-summary'],
             fixWebpackSourcePaths: true,
         },
