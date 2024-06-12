6ffa2ff48272085f1218c653a0344cf7e66ec074
Adrien Crivelli <adrien.crivelli@gmail.com>
2024-06-12 12:04:38 +0700

    Migrate away from modules in tests
    Instead, use providers, and avoid `imports` in
    `configureTestingModule()` even for the component being tested, because
    our code is exclusively standalone.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,4 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {FormsModule} from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatInputModule} from '@angular/material/input';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatCardModule} from '@angular/material/card';
 import {AppComponent} from './app.component';
 
 describe('AppComponent', () => {
@@ -17,22 +6,7 @@ describe('AppComponent', () => {
     let fixture: ComponentFixture<AppComponent>;
 
     beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [
-                MatIconModule,
-                MatButtonModule,
-                MatButtonToggleModule,
-                MatInputModule,
-                MatRadioModule,
-                MatCardModule,
-                MatCheckboxModule,
-                MatSlideToggleModule,
-                MatTooltipModule,
-                MatToolbarModule,
-                FormsModule,
-                AppComponent,
-            ],
-        }).compileComponents();
+        await TestBed.configureTestingModule({}).compileComponents();
         fixture = TestBed.createComponent(AppComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
