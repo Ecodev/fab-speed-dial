e41a1b4ec1ee58b81bb42b93aee7575fe8b2fe22
Adrien Crivelli <adrien.crivelli@gmail.com>
2025-02-03 16:17:37 +0900

    Animations are not needed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideZoneChangeDetection({eventCoalescing: true}), provideAnimationsAsync()],
+    providers: [provideZoneChangeDetection({eventCoalescing: true})],
 };
